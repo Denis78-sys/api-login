25bffe4cf61d9a3e80fe027a23176e2ae7a89991
Denis78-sys <denismarques736gmail.com>
2024-10-23 09:59:29 -0300

    ajustes

--- routes/private.js
+++ routes/private.js
@@ -4,6 +4,15 @@ import { PrismaClient } from "@prisma/client";
 const router = express.Router()
 const prisma = new PrismaClient()
 
+const app = express()
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
 //Criando rota GET
 router.get('/listar-usuarios', async (req, res) =>{
 
@@ -12,10 +21,12 @@ router.get('/listar-usuarios', async (req, res) =>{
         //Pegando dados dos usuários do banco menos a senha
         const users = await prisma.user.findMany()
 
-        res.status(200).json({message: "Usuários listados com sucesso!", users})
+        res.status(200).json(users)
     } catch (error) {
         res.status(500).json({message: "Erro no servidor"})
     }
+
+    
 })
 
 export default router
--- routes/public.js
+++ routes/public.js
@@ -5,13 +5,13 @@ import jwt from "jsonwebtoken";
 
 
 const app = express()
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
- */
+
 const prisma = new PrismaClient();
 const router = express.Router();
 //Puxando a variável do .ev
